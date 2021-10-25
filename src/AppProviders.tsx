aa93986a124227a3aa1828f26084f51c5f4a97c7
Alex Gabites <alex@netspeed.net.nz>
2021-10-26 01:11:33 +1300

    add color mode button

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,7 +1,27 @@
+import { IconButton } from '@chakra-ui/react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HiMoon, HiSun } from 'react-icons/hi';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Theme } from './components/Theme';
+import { useColorMode } from './hooks/useColorMode';
+
+const ToggleColorModeButton: React.FC = () => {
+  const { mode, toggleColorMode } = useColorMode();
+
+  return (
+    <IconButton
+      position="fixed"
+      bottom={2}
+      right={2}
+      aria-label="Toggle color mode"
+      colorScheme={mode('blackAlpha', 'yellow')}
+      icon={mode(<HiMoon />, <HiSun />)}
+      onClick={toggleColorMode}
+      size="sm"
+    />
+  );
+};
 
 export const AppProviders: React.FC = ({ children }) => (
   <HelmetProvider>
@@ -10,6 +30,7 @@ export const AppProviders: React.FC = ({ children }) => (
     <Theme>
       <Layout>
         <BrowserRouter>{children}</BrowserRouter>
+        <ToggleColorModeButton />
       </Layout>
     </Theme>
   </HelmetProvider>
